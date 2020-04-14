d6c979ea586ecbde3d02c4f30c47299780ed312c
Siosaia Fonua <sfonua10@churchofjesuschrist.org>
2020-04-13 22:16:31 -0600

    new mdx files

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,10 +4,11 @@ module.exports = {
       resolve: `gatsby-theme-blog`,
       options: {},
     },
+    `gatsby-plugin-mdx`,
   ],
   // Customize your site metadata:
   siteMetadata: {
-    title: `Gospel Insights`,
+    title: `Software Tech Blog`,
     author: `Saia Fonua`,
     description: `My site description...`,
     social: [
